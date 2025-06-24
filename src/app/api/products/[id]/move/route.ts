d1137ee2db9428e9bc612ac8452a83990f3d6e25
AnasHbaieb <anashbaieb@gmail.com>
2025-06-24 11:21:48 +0100

    test3 deploy

--- src/app/api/products/[id]/move/route.ts
+++ src/app/api/products/[id]/move/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse, NextRequest } from 'next/server'
 import { prisma } from '@/lib/prisma';
 
+export const dynamic = 'force-dynamic';
+
 interface Move{
   currentRang:number
   targetRang:number
