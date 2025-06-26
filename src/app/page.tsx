de065b7e255f9acb9611f16c567207c21e66a36d
AnasHbaieb <anashbaieb@gmail.com>
2025-06-26 23:53:03 +0100

    no thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -335,7 +335,7 @@ export default function Home() {
       {/* بطاقة التطوع */}
 <div className="bg-gray-50 p-8 rounded-lg shadow-md flex flex-col items-center text-center hover:shadow-xl transition">
   <div className="w-16 h-16 bg-green-100 rounded-full flex items-center justify-center mb-4">
-    {/* أيقونة تطوع جديدة (يدين تحمل قلب) */}
+    {/* أيقونة تطوع جديدة  */}
     <svg className="w-10 h-10 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
     </svg>
